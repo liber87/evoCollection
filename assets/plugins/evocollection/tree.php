8693c12bce5a4b4084f1419703c40126711e0bb4
liber87 <bd87@bk.ru>
2018-12-14 01:17:28 +0300

    fix for php 7.2.0

--- assets/plugins/evocollection/tree.php
+++ assets/plugins/evocollection/tree.php
@@ -4,8 +4,8 @@ if(!isset($_SESSION['mgrValidated'])){ die();}
 	$idsa = array();
 	foreach($configuration as $conf)
 	{
-		if (($conf['type']=='ids') and ($conf[value]) and ($conf['show_child']==0)) $idsa[]=$conf[value];			
-		if (($conf['type']=='template') and ($conf[value]) and ($conf['show_child']==0)) $idsa[] = $modx->db->getValue('Select GROUP_CONCAT(id) from '.$modx->getFullTableName('site_content').' where template in ('.$conf[value].')');
+		if (($conf['type']=='ids') and ($conf[value]) and ($conf['show_child']==0)) $idsa[]=$conf['value'];			
+		if (($conf['type']=='template') and ($conf[value]) and ($conf['show_child']==0)) $idsa[] = $modx->db->getValue('Select GROUP_CONCAT(id) from '.$modx->getFullTableName('site_content').' where template in ('.$conf['value'].')');
 	}
 	$ids = implode(',',$idsa);
 	if (!$ids) return;	
